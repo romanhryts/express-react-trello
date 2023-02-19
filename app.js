fc2f29954a03faf022c8b27fc7ae3afc1a61d6fc
Roman Hrytsyshyn <romangry.2004@gmail.com>
2023-02-19 22:09:08 +0100

    Fix CORS

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect(
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: [ 'http://localhost:3000', 'http://localhost:5173' ] }));
+app.use(cors({ credentials: true, origin: [ 'https://clever-arithmetic-8549f1.netlify.app/', 'http://localhost:5173' ] }));
 
 app.use('/lists', listsRouter);
 app.use('/card', cardRouter);
